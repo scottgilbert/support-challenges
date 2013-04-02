d269dd3c6404fb0a9a6b4db5798cefc93f982220
Scott Gilbert <scott.gilbert@rackspace.com>
2013-04-02 10:17:54 -0500

    Addition of challenge9

---
+++ challenge9.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# Challenge9 - Write an application that when passed the arguments FQDN,
+# image, and flavor it creates a server of the specified image and flavor with
+# the same name as the fqdn, and creates a DNS entry for the fqdn pointing to
+# the server's public IP.
+# Author: Scott Gilbert
+
+# Requires the following parameters:
+#  FQDN
+#  image uuid
+#  flavor
+
+import sys, os, re
+import pyrax
+import challenge1 as c1
+import challenge4 as c4
+
+credential_file=os.path.expanduser("~/.rackspace_cloud_credentials")
+pyrax.set_credential_file(credential_file)
+cs = pyrax.cloudservers
+dns = pyrax.cloud_dns
+
+def isValidImage(image):
+  try:
+    cs.images.get(image)
+    return True
+  except:
+    return False
+
+def isValidFlavor(flavor):
+  try:
+    cs.flavors.get(flavor)
+    return True
+  except:
+    return False
+
+def isValidHostname(hostname):
+  if len(hostname) > 255:
+    return False
+  if hostname[-1:] == ".":
+    hostname = hostname[:-1] # strip exactly one dot from the right, if present
+  allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+  return all(allowed.match(x) for x in hostname.split("."))
+
+def CloudServerPublicIPv4(server):
+  for addr in  server.networks['public']:
+    if c4.is_valid_ipv4_address(addr):
+      return addr
+  # if we don't find one, then return False
+  return False
+
+def CloudServerPublicIPv6(server):
+  for addr in  server.networks['public']:
+    if c4.is_valid_ipv6_address(addr):
+      return addr
+  # if we don't find one, then return False
+  return False
+
+
+if __name__ == "__main__":
+
+  # unbuffer stdout for pretty output
+  sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+  if len(sys.argv) == 4:
+    fqdn = sys.argv[1]
+    image = sys.argv[2]
+    flavor = sys.argv[3]
+    if not isValidHostname(fqdn):
+      print "This does not appear to be a valid image-uuid: %s" % image
+      sys.exit(2)
+    if not isValidImage(image):
+      print "This does not appear to be a valid image-uuid: %s" % image
+      sys.exit(3)
+    if not isValidFlavor(flavor):
+      print "This does not appear to be a valid flavor-id: %s" % flavor
+      sys.exit(4)
+
+    
+    servers = c1.BuildSomeServers(flavor, image, fqdn, 1)
+    c1.waitForServerNetworks(servers)
+    c1.printServersInfo(servers)
+    pubIPv4 = CloudServerPublicIPv4(servers[0])
+    c4.createDNSRecord(fqdn, pubIPv4, 'A')
+    pubIPv6 = CloudServerPublicIPv6(servers[0])
+    c4.createDNSRecord(fqdn, pubIPv6, 'AAAA')
+
+  else:
+    print "Wrong number of parameters specified!\n"
+    print "Usage:  challenge9 <FQDN> <image-uuid> <flavor-id>\n"
+    sys.exit(1)
+
+# vim: ts=2 sw=2 tw=78 expandtab
